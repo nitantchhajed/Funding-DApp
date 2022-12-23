00188d89e5ce7c053b2bb8007c805f7862e48587
Nitant <96972634+nitantchhajed@users.noreply.github.com>
2022-12-24 01:46:08 +0530

    Add files via upload

---
+++ app/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        epilogue: ['Epilogue', 'sans-serif'],
+      },
+      boxShadow: {
+        secondary: '10px 10px 20px rgba(2, 2, 2, 0.25)',
+      },
+    },
+  },
+  plugins: [],
+}
